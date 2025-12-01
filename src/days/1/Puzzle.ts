92a044bd3a8e53b85d5d813b8e5d39bd2231b71d
= <=>
2025-12-01 10:25:35 +0100

    day 1-1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,12 +1,52 @@
+type Action = { direction: 'L' | 'R'; count: number };
+
+function extractDirections(input: string): Action[] {
+  const splitedInput = input.split('\n');
+  return splitedInput.map((raw) => {
+    const [direction, ...number] = raw;
+    let parsedNumber = parseInt(number.join(''), 10);
+    while (parsedNumber > 100) {
+      parsedNumber = parsedNumber - 100;
+    }
+    return { direction, count: parsedNumber } as Action;
+  });
+}
+
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  const directions = extractDirections(input);
+  let position = 50;
+  let zeroCount = 0;
+  for (const { direction, count } of directions) {
+    if (direction === 'L') {
+      position -= count;
+      while (position < 0) {
+        position = 100 + position;
+      }
+    } else {
+      position += count;
+      while (position > 100) {
+        position = position - 100;
+      }
+
+      if (position === 100) {
+        position = 0;
+      }
+      if (position > 100) {
+        position = position - 100;
+      }
+    }
+    if (position === 0) {
+      zeroCount++;
+    }
+    console.log({ direction, count, position, zeroCount });
+  }
+  return zeroCount;
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 3;
 
 const second = (input: string) => {
-  console.log(input);
+  // console.log(input);
   return 'solution 2';
 };
 

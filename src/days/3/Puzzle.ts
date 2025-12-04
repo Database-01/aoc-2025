2697151c9ec7b9a2e354d568b4d5aa39299ddbff
Database-01 <206110461+Database-01@users.noreply.github.com>
2025-12-04 19:09:20 +0100

    day 3-2

--- src/days/3/Puzzle.ts
+++ src/days/3/Puzzle.ts
@@ -1,3 +1,7 @@
+function createJoltage(a: number, b: number): number {
+  return Number.parseInt(`${a}${b}`, 10);
+}
+
 function findJoltage(batteryBank: number[]): number {
   let max = 0;
   for (let i = 0; i < batteryBank.length - 1; i++) {
@@ -5,7 +9,7 @@ function findJoltage(batteryBank: number[]): number {
       if (i === j) {
         continue;
       }
-      const joltage = Number.parseInt(`${batteryBank[i]}${batteryBank[j]}`, 10);
+      const joltage = createJoltage(batteryBank[i], batteryBank[j]);
       if (joltage > max) {
         max = joltage;
       }
@@ -28,11 +32,29 @@ const first = (input: string) => {
 
 const expectedFirstSolution = 357;
 
+function findJoltage2(batteryBank: number[], depth = 12): number {
+  if (depth === 1) {
+    return Math.max(...batteryBank);
+  }
+  const splice = batteryBank.toSpliced(-(depth - 1));
+  const max = Math.max(...splice);
+  const index = splice.indexOf(max);
+  const nextSplice = [...batteryBank].toSpliced(0, index + 1);
+  const nestedJoltage = findJoltage2(nextSplice, depth - 1);
+  return createJoltage(max, nestedJoltage);
+}
+
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  const batteryBanks = input
+    .split('\n')
+    .map((line) => line.split('').map((v) => Number.parseInt(v, 10)));
+  const joltages: number[] = [];
+  for (const batteryBank of batteryBanks) {
+    joltages.push(findJoltage2(batteryBank));
+  }
+  return joltages.reduce((acc, joltage) => acc + joltage, 0);
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 3121910778619;
 
 export { expectedFirstSolution, expectedSecondSolution, first, second };

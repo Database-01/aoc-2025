8069f580d1e9386d7e0025451803e6b1d51841dc
Database-01 <206110461+Database-01@users.noreply.github.com>
2025-12-05 13:53:39 +0100

    day 5-2

--- src/days/5/Puzzle.ts
+++ src/days/5/Puzzle.ts
@@ -22,10 +22,38 @@ const first = (input: string) => {
 
 const expectedFirstSolution = 3;
 
+function rangeOverlap(a: Range, b: Range): boolean {
+  return (
+    (a.max >= b.min && a.max <= b.max) ||
+    (a.min >= b.min && a.min <= b.min) ||
+    (b.min >= a.min && b.min <= a.min) ||
+    (b.max >= a.min && b.max <= a.max)
+  );
+}
+
 const second = (input: string) => {
-  return 'solution 2';
+  const [rawRanges] = input.split('\n\n');
+  const freshRanges = getFreshRanges(rawRanges).toSorted((a, b) =>
+    a.min < b.min ? -1 : 1
+  );
+  const result = freshRanges.reduce((acc: Range[], range: Range) => {
+    const lastRange = acc.at(-1);
+    if (!lastRange) {
+      return [range];
+    }
+    if (!rangeOverlap(lastRange, range)) {
+      acc.push(range);
+      return acc;
+    }
+    lastRange.min = Math.min(lastRange.min, range.min);
+    lastRange.max = Math.max(lastRange.max, range.max);
+    return acc;
+  }, []);
+  return result.reduce((acc, range) => {
+    return acc + (range.max - range.min) + 1;
+  }, 0);
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 14;
 
 export { expectedFirstSolution, expectedSecondSolution, first, second };
